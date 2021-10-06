a084e2fabdee1dee1662e6cdfee229b5ddfa3a40
asebaq <ahmad_sebaq@yahoo.com>
2021-10-06 14:20:33 +0200

    Fix bug

--- Parser.py
+++ Parser.py
@@ -49,6 +49,9 @@ class XMLParser(Parser):
 
             self.result['transaction'][i]['vehicles'] = list()
 
+            if auto is None:
+                return self.result
+
             for j, vehicle in enumerate(auto.findall('Vehicle')):
                 self.result['transaction'][i]['vehicles'].append(dict())
 
